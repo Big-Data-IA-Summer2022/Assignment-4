80f94f54bdb0bf1b7eef42ac2c63fc1bf8f0b832
Abhijit Kunjiraman <95897040+Abhijitrk97@users.noreply.github.com>
2022-07-30 03:18:45 -0400

    Update model.py

--- pages/model.py
+++ pages/model.py
@@ -1,8 +1,5 @@
 from nbformat import write
 import streamlit as st
-import requests
-import json
-import tempfile
 from pathlib import Path
 
 def app():
@@ -12,7 +9,7 @@ def app():
     st.subheader("This model automates the application so we can trigger a benchmarking task from dataperf using Airflow")
     st.markdown('Please select values to get result :')
 
-    option = st.selectbox('Select Data_ID',('01g317-flipped','04hgtk-flipped','04rky-flipped','09j2d-flipped','01g317'))
+    option = st.selectbox('Select Data_ID',('01g317-flipped','04hgtk-flipped','04rky-flipped','09j2d-flipped','01g317','04rky','09j2d'))
     option1 = st.selectbox('Select train_size',('300',))     
     option2 = st.selectbox('Select noice_level',('0.3',)) 
     option3 = st.selectbox('Select test_size',('500',)) 
@@ -26,7 +23,7 @@ def app():
     if buttonstat:
         from jinja2 import Environment, FileSystemLoader
         
-        env = Environment(loader = FileSystemLoader(r"C:\Users\abhij\Documents\GitHub\assignment 4 stream\Assignment-4-streamlit_1\Assignment-4-streamlit_1\dags"), trim_blocks=True, lstrip_blocks=True)
+        env = Environment(loader = FileSystemLoader('./'), trim_blocks=True, lstrip_blocks=True)
 
         template = env.get_template('template.yml')
 
